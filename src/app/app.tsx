97bd2de1dddec3bbafd56cb54eb47be61a126d82
huyng12 <touch@nguyenrk.com>
2021-08-29 01:46:05 +0700

    Feat: Auto hide toolbar

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,6 @@
 import { set } from "idb-keyval";
 import * as monaco from "monaco-editor";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Editor as EditorComponent } from "./editor/editor";
 import { Editor as EditorType, EditorModel } from "./editor/type";
 import { readFile } from "../utils/file/read";
@@ -22,6 +22,7 @@ const loadFileToEditor = (
 export const App = () => {
 	const [handle, setHandle] = useState<FileSystemFileHandle | null>(null);
 	const [editor, setEditor] = useState<EditorType | null>(null);
+	const toolbarRef = useRef<HTMLDivElement>(null);
 
 	// Save handle to local
 	useEffect(() => {
@@ -36,10 +37,33 @@ export const App = () => {
 		return () => void dispose();
 	}, [handle, editor]);
 
+	const hideToolbar = () => toolbarRef.current?.classList.add(s.hideToolbar);
+	const showToolbar = () => toolbarRef.current?.classList.remove(s.hideToolbar);
+
+	// Show toolbar on mouseover
+	useEffect(() => {
+		if (toolbarRef === null) return;
+		toolbarRef.current?.addEventListener("mouseover", showToolbar);
+		return () =>
+			toolbarRef.current?.removeEventListener("mouseover", showToolbar);
+	}, [toolbarRef]);
+
+	// Hide toolbar when scrolling or typing
+	useEffect(() => {
+		if (editor === null) return;
+		const disposable = [
+			editor.onDidScrollChange(() => hideToolbar()),
+			editor.onDidChangeModelContent(() => hideToolbar()),
+		];
+		return () => disposable.forEach((d) => d.dispose());
+	}, [editor]);
+
 	return (
 		<div className={s.app}>
-			<div className={s.toolbar}>
-				<Toolbar editor={editor} handle={handle} setHandle={setHandle} />
+			<div ref={toolbarRef} className={s.toolbarWrapper}>
+				<div className={s.toolbar}>
+					<Toolbar editor={editor} handle={handle} setHandle={setHandle} />
+				</div>
 			</div>
 			<div className={s.editor}>
 				<EditorComponent setEditor={setEditor} />
