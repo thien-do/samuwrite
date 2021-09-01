6b545deb0709772085b4807dae0f348dfe08e39b
Thien Do <hi.thiendo@icloud.com>
2021-09-01 19:20:00 +0700

    Feat: Add basic support for theme

---
+++ src/components/prefs/prefs.tsx
@@ -0,0 +1,25 @@
+import { PrefsState } from "./state";
+import { Theme, THEMES } from "./theme/state";
+
+interface Props {
+	prefs: PrefsState;
+}
+
+export const Prefs = (props: Props): JSX.Element => {
+	return (
+		<div>
+			<select
+				value={props.prefs.theme}
+				onChange={(event) => {
+					props.prefs.setTheme(event.target.value as Theme);
+				}}
+			>
+				{THEMES.map((theme) => (
+					<option key={theme} value={theme}>
+						{theme}
+					</option>
+				))}
+			</select>
+		</div>
+	);
+};
