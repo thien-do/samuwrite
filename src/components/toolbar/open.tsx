d10474d96a09230e013da5aaf5d0d59bd9fe1f0b
Thien Do <hi.thiendo@icloud.com>
2021-09-20 09:44:45 +0700

    Feat: Add shortcuts for menu items

--- src/components/toolbar/open.tsx
+++ src/components/toolbar/open.tsx
@@ -1,14 +1,15 @@
 import { TippyProps } from "@tippyjs/react";
+import { useMemo } from "react";
 import { VscFolder } from "react-icons/vsc";
 import { Button } from "~/src/components/button/button";
 import { ButtonMoreMenuItem } from "~/src/components/button/more/menu";
-import { FileHandle, FileState } from "~/src/components/file/state";
+import { FileState } from "~/src/components/file/state";
 import { openFile } from "~src/app/utils/open";
+import { useShortcut } from "~src/components/shortcut/use-shortcut";
+import { SHORTCUTS } from "~src/components/toolbar/shortcuts";
+import { ERRORS } from "~src/utils/error";
 import { Editor } from "../editor/state/state";
 import { fileSystem } from "../file/system";
-import { SHORTCUTS } from "~src/components/toolbar/shortcuts";
-import { useShortcut } from "~src/components/shortcut/use-shortcut";
-import { useCallback } from "react";
 
 interface Props {
 	singleton: TippyProps["singleton"];
@@ -16,55 +17,71 @@ interface Props {
 	editor: Editor;
 }
 
-const menuRecent = (props: Props, recent: FileHandle): ButtonMoreMenuItem => ({
-	action: async () => {
-		const { editor, file } = props;
-		await openFile({ editor, file, handle: recent });
-	},
-	label: `Open "${recent.name}"`,
-	shortcut: SHORTCUTS.recent,
-});
+/**
+ * Memoized our callbacks so they can be used safely in effects
+ */
+const useOpenCallbacks = (props: Props) => {
+	const { editor, file } = props;
+	const callbacks = useMemo(() => {
+		const open = async () => {
+			const [handle] = await window.showOpenFilePicker({
+				multiple: false,
+				types: fileSystem.optionTypes,
+				excludeAcceptAllOption: false,
+			});
+			await openFile({ editor, file, handle });
+		};
+		const openNew = async () => {
+			console.log("ahihi");
+			await openFile({ editor, file, handle: null });
+		};
+		const openRecent = async () => {
+			const recent = file.recent;
+			if (recent === null) throw ERRORS.recentNull;
+			await openFile({ editor, file, handle: recent });
+		};
+		return { open, openNew, openRecent };
+	}, [editor, file]);
+	return callbacks;
+};
 
-const menuNew = (props: Props): ButtonMoreMenuItem => ({
-	action: async () => {
-		const { editor, file } = props;
-		await openFile({ editor, file, handle: null });
-	},
-	label: "New file",
-	shortcut: SHORTCUTS.new,
-});
+type Callbacks = ReturnType<typeof useOpenCallbacks>;
 
-const getMoreMenu = (props: Props): ButtonMoreMenuItem[] => {
-	const menu = [];
-	menu.push(menuNew(props));
+const getMoreMenu = (
+	props: Props,
+	callbacks: Callbacks
+): ButtonMoreMenuItem[] => {
+	const menu: ButtonMoreMenuItem[] = [];
+	menu.push({
+		action: callbacks.openNew,
+		label: "New file",
+		shortcut: SHORTCUTS.openNew,
+	});
 	if (props.file.recent !== null) {
-		menu.push(menuRecent(props, props.file.recent));
+		menu.push({
+			action: callbacks.openRecent,
+			label: `Open "${props.file.recent.name}"`,
+			shortcut: SHORTCUTS.openRecent,
+		});
 	}
 	return menu;
 };
 
 export const ToolbarOpen = (props: Props): JSX.Element => {
-	const { editor, file } = props;
+	const callbacks: Callbacks = useOpenCallbacks(props);
 
-	const open = useCallback(async () => {
-		const [handle] = await window.showOpenFilePicker({
-			multiple: false,
-			types: fileSystem.optionTypes,
-			excludeAcceptAllOption: false,
-		});
-		await openFile({ editor, file, handle });
-	}, [editor, file]);
-
-	useShortcut(SHORTCUTS.open, open);
+	useShortcut(SHORTCUTS.open, callbacks.open);
+	useShortcut(SHORTCUTS.openNew, callbacks.openNew);
+	useShortcut(SHORTCUTS.openRecent, callbacks.openRecent);
 
 	return (
 		<Button
-			onClick={open}
+			onClick={callbacks.open}
 			Icon={VscFolder}
 			shortcut={SHORTCUTS.open}
 			tooltip="Open…"
 			tooltipSingleton={props.singleton}
-			more={getMoreMenu(props)}
+			more={getMoreMenu(props, callbacks)}
 		/>
 	);
 };
