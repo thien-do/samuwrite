3b4912801316a741dff369490a8e18bddf989a99
Thien Do <hi.thiendo@icloud.com>
2021-09-20 19:16:48 +0700

    Fix: Fix toolbar width
    Previously, the toolbar (max) width is updated when the editor size is changed. This commit keeps the toolbar width unchanged if the prefs panel is visible. This prevents the layout shift when the user changes the editor size, especially via a slider.

--- src/components/prefs/state.ts
+++ src/components/prefs/state.ts
@@ -2,11 +2,13 @@ import { LayoutState, usePrefsLayout } from "./layout";
 import { SizeState, usePrefsSize } from "./size/state";
 import { ThemeState, useThemeState } from "../theme/state";
 import { VimState, usePrefsVim } from "./vim";
+import { PrefsVisibleState, usePrefsVisibleState } from "./prefs-visible";
 
 export interface PrefsState
 	extends ThemeState,
 		VimState,
 		LayoutState,
+		PrefsVisibleState,
 		SizeState {}
 
 export const usePrefs = (): PrefsState => {
@@ -14,5 +16,6 @@ export const usePrefs = (): PrefsState => {
 	const vim = usePrefsVim();
 	const layout = usePrefsLayout();
 	const size = usePrefsSize();
-	return { ...theme, ...vim, ...layout, ...size };
+	const prefsVisible = usePrefsVisibleState();
+	return { ...theme, ...vim, ...layout, ...prefsVisible, ...size };
 };
