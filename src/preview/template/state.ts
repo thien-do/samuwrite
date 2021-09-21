1248739f7b7337f6d528a14f8299bf620806ff63
Thien Do <hi.thiendo@icloud.com>
2021-09-21 11:05:36 +0700

    Fix: Improve GitHub template

--- src/preview/template/state.ts
+++ src/preview/template/state.ts
@@ -1,8 +1,6 @@
 import { useStorageState } from "~src/utils/state/storage";
 import { SetState } from "~src/utils/state/type";
-import serif from "./styles/serif/serif";
-import github from "./styles/github.module.css";
-import tailwind from "./styles/tailwind/tailwind";
+import * as styles from "./styles/styles";
 import { EN_DASH } from "~src/utils/typography";
 
 export const PREVIEW_TEMPLATE_NAMES = ["tailwind", "github", "serif"] as const;
@@ -23,9 +21,18 @@ export const PREVIEW_TEMPLATE_DETAILS: Record<
 	PreviewTemplateName,
 	PreviewTemplateDetail
 > = {
-	github: { name: `Sans ${EN_DASH} GitHub`, className: github.container },
-	tailwind: { name: `Sans ${EN_DASH} Tailwind`, className: tailwind.container },
-	serif: { name: "Serif", className: serif.container },
+	github: {
+		name: `Sans ${EN_DASH} GitHub`,
+		className: styles.github.container,
+	},
+	tailwind: {
+		name: `Sans ${EN_DASH} Tailwind`,
+		className: styles.tailwind.container,
+	},
+	serif: {
+		name: "Serif",
+		className: styles.serif.container,
+	},
 };
 
 export const usePreviewTemplateState = (): PreviewTemplateState => {
---
+++ src/preview/template/styles/styles.ts
@@ -0,0 +1,3 @@
+export { default as github } from "./github/github";
+export { default as serif } from "./serif/serif";
+export { default as tailwind } from "./tailwind/tailwind";
