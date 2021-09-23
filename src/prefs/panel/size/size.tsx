1106eb65026728650301d15d6cca59bc5e525ed6
Thien Do <hi.thiendo@icloud.com>
2021-09-23 15:39:48 +0700

    Feat: Add range input for editor size

--- src/prefs/panel/size/size.tsx
+++ src/prefs/panel/size/size.tsx
@@ -10,22 +10,27 @@ interface Props {
 export const SizePref = (props: Props): JSX.Element => {
 	return (
 		<div className={s.container}>
-			<label className={s.label}>
-				<span className={s.labelText}>Editor size</span>
-				<div className={s.select}>
-					<Select<string>
-						value={props.prefs.size}
-						setValue={(value) => props.prefs.setSize(value as SizeName)}
-						options={SIZE_NAMES.map((size) => ({
-							id: size,
-							label: size,
-							value: size,
-						}))}
-						fill
-						autoFocus
-					/>
-				</div>
-			</label>
+			<Select<string>
+				value={props.prefs.size}
+				setValue={(value) => props.prefs.setSize(value as SizeName)}
+				options={SIZE_NAMES.map((size) => ({
+					id: size,
+					label: size,
+					value: size,
+				}))}
+				fill
+				autoFocus
+			/>
+			<input
+				type="range"
+				min={0}
+				max={SIZE_NAMES.length}
+				value={SIZE_NAMES.indexOf(props.prefs.size)}
+				onChange={(event) => {
+					const size = SIZE_NAMES[event.target.valueAsNumber];
+					props.prefs.setSize(size);
+				}}
+			/>
 		</div>
 	);
 };
