a4fee14615064ea1c12ec0a263233fab721b143b
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-06-24 23:28:04 +0700

    a

---
+++ src/app/main/main.tsx
@@ -0,0 +1,34 @@
+// import { useEditor } from "~src/editor/state/state";
+// import { Layout } from "~src/layout/layout";
+// import { Toolbar } from "~src/toolbar/toolbar";
+// import { usePrefs } from "~src/prefs/state";
+// import { ThemeInject } from "~src/theme/inject";
+import * as s from "./main.module.css";
+// import { useToolbarAutohide } from "./toolbar/autohide";
+
+export const Main = (): JSX.Element => {
+  // const editor = useEditor();
+  // const prefs = usePrefs();
+  // const toolbar = useToolbarAutohide({ editor });
+
+  return (
+    <div className={s.app}>
+      hello
+      {/* <ThemeInject theme={prefs.theme} /> */}
+      {/* It is actually ok to render Toolbar when editor is null (not
+				initialized). However, most actions in Toolbar depends on the
+				editor so it's simpler to just check for null here once */}
+      {/* {editor.value !== null && (
+        <div
+          className={[s.toolbar, toolbar.mute ? s.muted : ""].join(" ")}
+          {...toolbar.handlers}
+        >
+          <Toolbar show={toolbar.show} editor={editor.value} prefs={prefs} />
+        </div>
+      )} */}
+      <div className={s.body}>
+        {/* <Layout editor={editor} prefs={prefs} /> */}
+      </div>
+    </div>
+  );
+};
--- src/app/public/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as ReactDom from "react-dom";
-import { App } from "../app/app";
-import "./style/style.ts";
-
-const container = document.getElementById("app");
-if (container === null) throw Error("`#app` is null");
-ReactDom.render(<App />, container);
